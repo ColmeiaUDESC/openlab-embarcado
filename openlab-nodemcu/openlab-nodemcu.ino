684cd95f24e71fbc674f0f6f9ac23bb1dfe0954f
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2022-01-26 22:14:11 -0300

    internet connection established

---
+++ openlab-nodemcu/openlab-nodemcu.ino
@@ -0,0 +1,33 @@
+#include <ESP8266WiFi.h>
+#define timeout_limit (1000 * 30)
+#define __SSID__ ""
+#define __PSWD__ ""
+
+int timeout_check = 0;
+
+
+void setup(){
+  WiFi.begin(__SSID__, __PSWD__);
+  
+  Serial.begin(115200);
+  Serial.print("\nConnecting");
+  
+  while (WiFi.status() != WL_CONNECTED){
+    if (timeout_check <= timeout_limit){
+      delay(500);
+      Serial.print(".");
+      timeout_check += 500;
+    }else{
+      Serial.print("\nConnection Timeout");
+      break;
+    }
+  }
+  
+  if (WiFi.status() == WL_CONNECTED){
+    Serial.print("\nConnected, IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+  
+}
+
+void loop() {}
